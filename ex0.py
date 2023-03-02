e74213960fafee75f37ede9f2d7fade8f10a24ba
perrenick <126802488+perrenick@users.noreply.github.com>
2023-03-02 21:45:37 +0200

    Add files via upload

--- ex0.py
+++ ex0.py
@@ -1,19 +1,25 @@
-import requests  # εισαγωγή της βιβλιοθήκης
+import requests
+
+def get_url():
+    url = input("Παρακαλώ εισάγετε ένα URL: ")
+    return url
+
+url = get_url()
+
+response = requests.get(url)
+
+print("Οι κεφαλίδες είναι:")
+for header, value in response.headers.items():
+    print(header + ": " + value)
 
 
-def more(text):
-    count = 0
-    for line in text.split('\n'):
-        print(line)
-        count += 1
-        if count % 30 == 0:
-            reply = input('Show more (y/n)? ')
-            if reply == 'n':
-                break
+#Για τον server που χρησιμοποιω:
+print("Το λογισμικο του web server είναι: {}".format(response.headers['Server']))
+
+#Για τα cookies
+A = response.headers['Set-Cookie'].split(';')
+for i in range(len(A)):
+    if 'expires' in A[i]:
+        print("Τo cookie {} , {}".format(A[i-1],A[i]))
 
 
-url = 'http://python.org/'  # προσδιορισμός του url
-
-with requests.get(url) as response:  # το αντικείμενο response
-    html = response.text
-    more(html)
